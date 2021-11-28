11bc89351480acca335c6777a3e28156bd20e359
caroline <cluce059@gmail.com>
2021-11-27 21:13:05 -0500

    arch

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
 import Home from './pages/Home';
-import Project from './pages/Home';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer';
 import NoMatch from './pages/NoMatch';
+import Projects from'./pages/Projects';
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-    <Router>
-      <>
-        <Navbar />
+    <>
+      <Navbar />
+        <Router>
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/Projects' component={Projects}/>
           <Route component={NoMatch} />
-        </Switch>
-      </>
-    </Router>
-    <Footer />
-    </ApolloProvider>
+          </Switch>
+        </Router>
+     <Footer />
+    </>
   )
 };
 
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import {  Container, Card, CardColumns, Form, Button, Col } from 'react-bootstrap';
-
+import About from '../components/About';
 
 const Home = () => {
 
     return (
         <>
         <Container>
-            
+            <About />
         </Container>
         </>
     )
